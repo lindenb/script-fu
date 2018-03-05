365784ef22fc257a3d24252f6809fe6ad718347d
lindenb <plindenbaum@yahoo.fr>
2018-03-05 17:36:25 +0100

    cont

--- mod.utils.scm
+++ mod.utils.scm
@@ -1,2 +1,63 @@
-(define (my-display-image img)  (gimp-display-new img) )
+(define pi  (acos -1))
+(define pi2  (* 2.0 pi))
+(define RAND_MAX 1000000)
+;;
+;; returns number between 0.0 and 1.0
+;; 
+(define rnd  (lambda () (/ (remainder (random-next) RAND_MAX)  RAND_MAX ) ))
+;;
+;; returns boolean true or false
+;; 
+(define random-boolean  (lambda () (if (< (rnd) 0.5) #t #f)))
+;;
+;; returns number -1 or 1
+;; 
+(define random-sign  (lambda () (if (random-boolean) -1 1)))
+;;
+;; perimeter of a circle
+;;
+(define circle-perimeter  (lambda (r) (* pi2 r) ) )
+
+
+(define (my-select-sketchy-circle image selmode cx cy r) 
+	(let*
+		(
+		(angle-start (* (rnd)  pi2))
+		(delta-angle (* (rnd)  pi2))
+		(perimeter (* r  pi2))
+		(angle 0)
+		(segs (list )))
+		)
+	(set! segs (make-vector 0))
+
+	(gimp-image-select-polygon image selmode num-segs segs)
+	)
+)
+ 
+
+(let*
+		(
+		(cx 0)
+		(cy 0)
+		(radius 100.0)
+		(precision 5.0)
+		(nsteps (max 3 (floor (/ (circle-perimeter radius) precision ) ) ) )
+		(angle-per-step (/pi2 nsteps))
+		(angle 0)
+		(segs  (list) ))
+		(n 0)
+		)
+		
+  (while (< n nsteps) 
+  (let * 
+  	(
+  	(r2 (max 0 radius ))
+  	) 
+  	
+  ) 
+  
+  (set! n (+ n 1))
+  )
+ (display "done\n")
+ )
 
---
+++ resize-tumblr.scm
@@ -0,0 +1,34 @@
+;; #######################################################################################################
+
+(define
+	(script-fu-resize-tumblr01 image)
+	 (let* (
+	 	 (SIZE 1280)
+	 	 (W (car (gimp-image-width image)) )
+	 	 (H (car (gimp-image-height image)) )
+	 	 (W2 (if (< W H) (* (/ W H) SIZE) SIZE))
+	 	 (H2 (if (< W H) SIZE (* (/ H W) SIZE )))
+	 	)
+	 (gimp-image-undo-group-start image)
+	 (gimp-image-flatten image)
+	 (gimp-image-scale image W2 H2)
+	 (gimp-image-resize SIZE SIZE (- SIZE  (/ W2 2)) (- SIZE  (/ H2 2)))
+	 (gimp-image-undo-group-end image)
+	 (gimp-displays-flush)
+	 )
+)
+
+;; #######################################################################################################
+(script-fu-register "script-fu-resize-tumblr01"
+  _"ResizeTumblr..."
+  "ResizeTumblr"
+  "Pierre Lindenbaum"
+  "(c) Pierre Lindenbaum"
+  "2018"
+  ""
+ SF-IMAGE      "Image"     0
+)
+
+
+(script-fu-menu-register "script-fu-resize-tumblr01"
+                         "<Toolbox>/Xtns/Languages/Script-Fu/Yokofakun")
