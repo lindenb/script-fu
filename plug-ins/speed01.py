fd092bcdda1780e3c080d9711f9c78de22d183a1
Pierre Lindenbaum <plindenbaum@yahoo.fr>
2018-03-07 20:09:07 +0100

    cont

--- plug-ins/speed01.py
+++ plug-ins/speed01.py
@@ -4,6 +4,7 @@
 
 from gimpfu import *
 import sys
+import random
 import gimputils
 
 
@@ -11,15 +12,31 @@ import gimputils
 class Speed01(gimputils.AbstractGimpPlugin):
 	pass
 
-
-def speed01(image, drawable, initstr, font, size, color) :
+def speed01(image, drawable) :
 	self = Speed01()
-	self.width = drawable.width
-        self.height = drawable.height
+	x1 = 0
+	y1 = 0
+	x2 = drawable.width
+	y2 = drawable.height
+	proba = 0.001
+	npoints = proba * (( x2 - x1 ) * (y2 - y1) )
 	pdb.gimp_context_push()
-	self.log( gimputils.add(10,17)  )
-	gimp.displays_flush()
+	pdb.gimp_progress_init("SPeed01", None)
+	pdb.gimp_undo_push_group_start(image)
+	selection = pdb.gimp_image_get_selection(image)
+	gimp.set_foreground(0,0,0)
+	bg = pdb.gimp_image_get_active_drawable(image);
+	while npoints > 0:
+		cx = random.randrange(x1,x2)
+		cy = random.randrange(y1,y2)
+		radius =  random.randrange(2,10)
+		pdb.gimp_image_select_item(image,2,selection)
+		pdb.gimp_ellipse_select(image, cx - radius, cy - radius, 2 * radius, 2 * radius, 0, True, False, 0)
+		pdb.gimp_bucket_fill( bg, 0,0, 100,0,False,0,0)
+		npoints -= 1
+	pdb.gimp_undo_push_group_end(image)
 	pdb.gimp_context_pop()
+	pdb.gimp_displays_flush()
 
 register(
     "python_fu_speed01",
@@ -32,11 +49,7 @@ register(
     "",      # Create a new image, don't work on an existing one
     [
    	(PF_IMAGE, "image", "Input image", None),
-        (PF_DRAWABLE, "drawable", "Input drawable", None),
-        (PF_STRING, "string", "Text string", 'Hello, world!'),
-        (PF_FONT, "font", "Font face", "Sans"),
-        (PF_SPINNER, "size", "Font size", 50, (1, 3000, 1)),
-        (PF_COLOR, "color", "Text color", (1.0, 0.0, 0.0))
+    (PF_DRAWABLE, "drawable", "Input drawable", None)
     ],
     [],
     speed01, menu= "<Toolbox>/Xtns/Languages/Script-Fu/Yokofakun" 
